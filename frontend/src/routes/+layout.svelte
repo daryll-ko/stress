ba49d8d6d8b5d1bf2c825c0ffed8236f08caba3d
Daryll Ko <daryll.ko@gmail.com>
2024-05-27 10:43:53 +0800

    add: make basic layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="h-screen p-10">
+	<slot />
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let balance: number;
+	let shouts: number;
+	let steps: number;
+	let meditations: number;
+	let awards: number;
+	let achievements: number;
+	let places: number;
+</script>
+
+<h1 class="text-3xl font-bold">Stress</h1>
+<br />
+<div class="flex flex-col items-center gap-3">
+	<form class="flex flex-col gap-3 items-start bg-orange-100 p-3 rounded-lg w-[50%]">
+		<div class="question">
+			How often do you shout daily?
+			<input bind:value={shouts} type="number" min={0} class="input" />
+		</div>
+		<div class="question">
+			How many steps do you walk in a day?
+			<input bind:value={steps} type="number" min={0} class="input" />
+		</div>
+		<div class="question">
+			How often do you meditate in a week?
+			<input bind:value={meditations} type="number" min={0} class="input" />
+		</div>
+		<div class="question">
+			How many personal awards do you have?
+			<input bind:value={awards} type="number" min={0} class="input" />
+		</div>
+		<div class="question">
+			How many achievements are you proud of?
+			<input bind:value={achievements} type="number" min={0} class="input" />
+		</div>
+		<div class="question">
+			How many places have you visited over the past week?
+			<input bind:value={places} type="number" min={0} class="input" />
+		</div>
+	</form>
+	<button class="bg-orange-100 rounded-md px-3 py-2 border-2 border-solid border-orange-400 hover:scale-110 transition-all">Submit</button>
+</div>
